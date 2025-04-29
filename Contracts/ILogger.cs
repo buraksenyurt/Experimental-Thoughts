365e996fd85cd125f2fb4cbc00c3e381804364ed
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2025-04-29 11:21:05 +0000

    Initiated solution

---
+++ Contracts/ILogger.cs
@@ -0,0 +1,9 @@
+namespace Contracts;
+
+public interface ILogger
+{
+    void Info(string message);
+    void Debug(string message);
+    void Error(string message, Exception exception);
+    void Warn(string message);
+}
